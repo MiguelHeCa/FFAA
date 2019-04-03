464fed63a72cac8ff4564d463ec35b14b247e934
Miguel Hernandez <jhernandcab@gmail.com>
2019-04-02 19:27:06 -0600

    Adding regions to countries

--- military_expenditure.R
+++ military_expenditure.R
@@ -11,3 +11,16 @@ raw_data <- wb(
   POSIXct = TRUE)
 
 countries <- wbcountries(lang = "en")
+
+country_id <- countries %>% 
+  select(iso3c, country, regionID, region, incomeID, income)
+
+tidy_data <- raw_data %>% 
+  select(iso3c, date_ct, country, value)
+
+mil_exp <- tidy_data %>% left_join(country_id, by = c("iso3c", "country")) %>% as_tibble()
+
+map(mil_exp, ~sum(is.na(.)))
+
+mil_exp %>% 
+  ggplot()
